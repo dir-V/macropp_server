9c95cc587d74281c7f9f8aeefaa2297546ee5a9f
Tom Peace <thomasjpeace29@gmail.com>
2025-12-17 11:13:19 +0000

    feat(food log service): add food log service method to add food log

---
+++ src/main/kotlin/com/healthfit/macroplus/services/FoodLogService.kt
@@ -0,0 +1,86 @@
+package com.healthfit.macroplus.services
+
+import com.healthfit.macroplus.models.Food
+import com.healthfit.macroplus.models.FoodLog
+import com.healthfit.macroplus.repositories.FoodLogRepository
+import com.healthfit.macroplus.repositories.FoodRepository
+import com.healthfit.macroplus.repositories.UserRepository
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.NoSuchElementException
+import java.util.UUID
+
+@Service
+open class FoodLogService(
+	private val foodLogRepository: FoodLogRepository,
+	private val foodRepository: FoodRepository,
+	private val userRepository : UserRepository
+	) {
+
+	@Transactional
+	 open fun addFoodLogWithFoodId(
+		userId: UUID,
+		foodId: UUID,
+		name: String,
+		quantityGrams: BigDecimal,
+		loggedAt: LocalDateTime? = LocalDateTime.now(),
+		): FoodLog {
+
+		val foundUser = userRepository.findById(userId)
+			.orElseThrow { NoSuchElementException("User not found with ID: $userId") }
+
+		val foundFood = foodRepository.findById(foodId)
+			.orElseThrow { NoSuchElementException("Food not found with ID: $foodId") }
+
+		val ratio = quantityGrams.divide(BigDecimal("100"))
+		val calories = (foundFood.caloriesPer100Grams * ratio.toDouble()).toInt()
+
+		val proteinGrams = foundFood.proteinPer100Grams?.multiply(ratio)
+		val carbsGrams = foundFood.carbsPer100Grams?.multiply(ratio)
+		val fatsGrams = foundFood.fatsPer100Grams?.multiply(ratio)
+
+		val newFoodLog = FoodLog(
+			user = foundUser,
+			food = foundFood,
+			name = name,
+			quantityGrams = quantityGrams,
+			loggedAt = loggedAt,
+			calories = calories,
+			proteinGrams = proteinGrams,
+			carbsGrams = carbsGrams,
+			fatsGrams = fatsGrams
+		)
+
+		return foodLogRepository.save(newFoodLog)
+	}
+
+
+	@Transactional
+	 open fun addFoodLogQuickAdd(
+		userId: UUID,
+		calories: Int,
+		loggedAt: LocalDateTime? = LocalDateTime.now(),
+		): FoodLog {
+
+		val foundUser = userRepository.findById(userId)
+			.orElseThrow { NoSuchElementException("User not found with ID: $userId") }
+
+		val newFoodLog = FoodLog(
+			user = foundUser,
+			food = null,
+			name = "Quick Add",
+			quantityGrams = null,
+			loggedAt = loggedAt,
+			calories = calories,
+			proteinGrams = BigDecimal.ZERO,
+			carbsGrams = BigDecimal.ZERO,
+			fatsGrams = BigDecimal.ZERO
+		)
+
+		return foodLogRepository.save(newFoodLog)
+	}
+
+}
